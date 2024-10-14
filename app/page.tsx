c62fd5f0ed687d44bcab1740b1425f7e9262fa88
Muhammad Umar <muhammadumar695@gmail.com>
2024-10-15 02:49:07 +0500

    feat: added a provider component in which a next-theme-provider is added, additionally text generate effect of aceternity ui is added which used framer-motion, clsx and tailwind-merge. Overall, the dotted grid and text effects are looking really cool.

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10">
-      <div className="max-w-7xl w-full bg-black-100">
-        <Hero/>
+    <main className="w-full h-auto">
+      <div className="relative flex flex-col justify-center items-center" >
+        <Hero />
       </div>
+      <div className="mt-[100vh]" />
+      <div> Hello Mate </div>
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36' >
+    <div className='h-screen w-full flex items-center justify-center absolute top-0 left-0 overflow-x-hidden' >
+
       <div>
-        < Spotlight fill='white' className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' />
-        < Spotlight fill='purple' className='top-10 left-full h-[80vh] w-[50vw]' />
-        < Spotlight fill='blue' className='-top-28 left-80 h-[80vh] w-[50vw]' />
+        <Spotlight
+          fill='white'
+          className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' />
+        <Spotlight
+          fill='purple'
+          className='top-10 left-full h-[80vh] w-[50vw]' />
+        <Spotlight
+          fill='blue'
+          className='-top-28 left-80 h-[80vh] w-[50vw]' />
+        <Spotlight
+          fill='blue'
+          className='-top-28 left-50 h-[80vh] w-[50vw]' />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+
+        <div className='w-full flex-col lg:flex-row flex justify-center my-20 z-10 border-2 border-red-500' >
+          <div className='w-[50%] flex flex-col items-center justify-center border-2 border-green-500' >
+            < TextGenerateEffect
+              className='uppercase tracking-widest text-center text-4xl md:text-5xl lg:text-6xl'
+              words="Hello, I'm Muhammad Umar" 
+              />
+            <p className="uppercase tracking-widest text-lg md:text-2xl text-center text-white">
+              I&apos;m a full-stack developer
+            </p>
+          </div>
+          <div className='w-[50%] border-2 border-pink-500' >
+              Image Here
+          </div>
+        </div>
+
       </div>
+
     </div>
   )
 }
